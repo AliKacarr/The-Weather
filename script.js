c2a40e0c1e58438c40f792b51dff828f06f075b7
EsraGumus7 <esragumus7788@gmail.com>
2024-12-19 20:52:29 +0300

    menu eklendi

--- script.js
+++ script.js
@@ -565,4 +565,21 @@ async function updateVisitedCities(city) {
     }
 }
 
+//----------------------------------------------
 
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "250px";
+    document.getElementById("sideMenu").style.display="block";
+    document.querySelector(".menu-toggle").style.display = "none"; // Açma butonunu gizle
+    const menuToggle = document.querySelector(".menu-toggle");
+    menuToggle.classList.add("hidden"); // Açma butonunu gizle
+  }
+  
+  function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+    document.querySelector(".menu-toggle").style.display = "block"; // Açma butonunu göster
+    setTimeout(() => {
+        const menuToggle = document.querySelector(".menu-toggle");
+        menuToggle.classList.remove("hidden"); // Açma butonunu yavaşça göster
+    }, 600);
+  }
